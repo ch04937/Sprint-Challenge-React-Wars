d09a2edca173a933a8db1887b44749c345966ff0
ch04937 <97hernandez.c@gmail.com>
2019-07-19 08:54:52 -0700

    'added api to app and started creating components'

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 const App = () => {
@@ -8,10 +10,23 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [people, setPeople] = useState(null);
+  useEffect(() => {
+    axios.get(
+      'https://swapi.co/api/people'
+    )
+    .then(res => {
+      setData(res.people)
+    });
+  }, []);
+
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <h1 className="Header">React Wars Handbook</h1>
+      {people
+      ? <StarWars /> 
+    : <div>Loading</div>}
     </div>
   );
 }
